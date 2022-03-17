1e6ba68b6207217e84b06113d5330bb602e48b8d
Hilmi Can Taşkıran <hilmicantaskiran@gmail.com>
2022-03-18 00:09:25 +0300

    Create form and add bulma.css

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <form method="post">
+            <div class="subtitle subtitle-margin">Fatura bilgilerini giriniz.</div>
+            <div class="field">
+                <label class="label">Alıcı İsmi</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Text input" required oninvalid="this.setCustomValidity('Lütfen alıcı ismini giriniz.')" oninput="setCustomValidity('')">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Alıcı İsmi</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Text input" required oninvalid="this.setCustomValidity('Lütfen alıcı ismini giriniz.')" oninput="setCustomValidity('')"> 
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Alıcı İsmi</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Text input" required oninvalid="this.setCustomValidity('Lütfen alıcı ismini giriniz.')" oninput="setCustomValidity('')">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Alıcı İsmi</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Text input" required oninvalid="this.setCustomValidity('Lütfen alıcı ismini giriniz.')" oninput="setCustomValidity('')">
+                </div>
+            </div>
+            <div class="control">
+                <input class="button is-link button-size" type="submit" value="Gönder">
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import "@/assets/styles/bulma.css";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App'
 }
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    display: flex;
+    flex-direction: column;
+    margin: 10px;
+}
+
+.subtitle-margin {
+    margin-top: 1rem;
+}
+
+@media screen and (max-width: 768px) {
+    #app {
+        margin: 10px;
+    }
+
+    .button-size {
+        margin-top: 10px;
+        width: 100%;
+    }   
+}
+
+@media screen and (min-width: 769px) {
+    #app {
+        margin: 50px 100px 0px 100px;    
+    }
+
+    .button-size {
+        margin-top: 10px;
+    }
 }
 </style>
